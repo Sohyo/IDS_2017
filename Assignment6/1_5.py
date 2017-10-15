a2cf6696b0b40c75bca87bb8aca75b5bf65a7457
Daniel Mark Low <danielmlow@RN-145-90-134-100.eduroam.rug.nl>
2017-10-15 12:20:24 +0200

    Fixed bug in 1_5.py

--- Assignment6/1_5.py
+++ Assignment6/1_5.py
@@ -21,6 +21,9 @@ path_freq = path+'term_frequency/'
 save_fig_pca = '1_5_pca.png'
 titles = gutenberg.fileids()
 
+
+# Take 300 highest freq words from each book. Add to dataframe.
+
 df = pd.read_csv(path_freq + titles[0]+'.csv',header=None)   
 
 df1 = df.iloc[:300,1]
@@ -59,9 +62,9 @@ df0 = df0*100000
 for color, i, target_name in zip(colors, range(18), np.array(titles)):
     plt.scatter(df0.iloc[i,0], df0.iloc[i,1], color=color, alpha=.8,label=y)
 
-
-for i, txt in enumerate(titles):
-    plt.annotate(txt, (df0.iloc[i,0],df0.iloc[i,1]),rotation=45,size=4,c='g')
+# If you want text on dots instead of one legend
+# for i, txt in enumerate(titles):
+#     plt.annotate(txt, (df0.iloc[i,0],df0.iloc[i,1]),rotation=45,size=4)
 
 plt.xlim(-0.1,0.2)
 plt.ylim(-0.1,0.2)
@@ -69,7 +72,7 @@ plt.legend(titles, loc='center left', bbox_to_anchor=(1, 0.5))
 plt.xlabel('x-PCA')
 plt.ylabel('y-PCA')
 plt.title('PCA of vector semantics for top 300 words from each book')
-plt.savefig(path+save_fig_pca)
-plt.show()
+plt.savefig(path+save_fig_pca, bbox_inches='tight')
+# plt.show()
 
 
